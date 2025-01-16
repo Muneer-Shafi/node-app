ad0814e5bfad00c41577d3aca1e90adaaf6f1a7b
Muneer shafi <zmzahidmuneer@gmail.com>
2025-01-16 18:37:11 +0530

    feat: Add employees and students data to API endpoints

---
+++ src/data/data.ts
@@ -0,0 +1,64 @@
+export const employees = [
+  {
+    id: 1,
+    name: "John Doe",
+    age: 30,
+    position: "Software Engineer",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    age: 28,
+    position: "Software Engineer",
+  },
+  {
+    id: 3,
+    name: "Tom Smith",
+    age: 35,
+    position: "Project Manager",
+  },
+  {
+    id: 4,
+    name: "Jerry Johnson",
+    age: 40,
+    position: "Software Engineer",
+  },
+  {
+    id: 5,
+    name: "Sara Johnson",
+    age: 35,
+    position: "Software Engineer",
+  },
+];
+export const students = [
+  {
+    id: 1,
+    name: "John Doe",
+    age: 20,
+    grade: "A",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    age: 22,
+    grade: "B",
+  },
+  {
+    id: 3,
+    name: "Tom Smith",
+    age: 21,
+    grade: "A",
+  },
+  {
+    id: 4,
+    name: "Jerry Johnson",
+    age: 23,
+    grade: "B",
+  },
+  {
+    id: 5,
+    name: "Sara Johnson",
+    age: 22,
+    grade: "A",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,7 @@
 import express, { Request, Response } from "express";
+import { employees, students } from "./data/data";
 
-const employees = [
-  {
-    id: 1,
-    name: "John Doe",
-    age: 30,
-    position: "Software Engineer",
-  },
-  {
-    id: 2,
-    name: "Jane Doe",
-    age: 28,
-    position: "Software Engineer",
-  },
-  {
-    id: 3,
-    name: "Tom Smith",
-    age: 35,
-    position: "Project Manager",
-  },
-  {
-    id: 4,
-    name: "Jerry Johnson",
-    age: 40,
-    position: "Software Engineer",
-  },
-  {
-    id: 5,
-    name: "Sara Johnson",
-    age: 35,
-    position: "Software Engineer",
-  },
-];
-const students = [
-  {
-    id: 1,
-    name: "John Doe",
-    age: 20,
-    grade: "A",
-  },
-  {
-    id: 2,
-    name: "Jane Doe",
-    age: 22,
-    grade: "B",
-  },
-  {
-    id: 3,
-    name: "Tom Smith",
-    age: 21,
-    grade: "A",
-  },
-  {
-    id: 4,
-    name: "Jerry Johnson",
-    age: 23,
-    grade: "B",
-  },
-  {
-    id: 5,
-    name: "Sara Johnson",
-    age: 22,
-    grade: "A",
-  },
-];
+
 
 const app = express();
 const PORT = 8000;
